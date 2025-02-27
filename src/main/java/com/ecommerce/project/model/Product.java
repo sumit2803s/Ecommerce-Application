3a4515dc2b43f2ce3148482638fceaa25f057e79
ACER <sumit.jaiswal8888@gmail.com>
2025-02-27 16:57:57 +0530

    Implementing User Profiling

--- src/main/java/com/ecommerce/project/model/Product.java
+++ src/main/java/com/ecommerce/project/model/Product.java
@@ -6,11 +6,14 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "products")
+@ToString
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -29,4 +32,8 @@ public class Product {
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private User user;
 }
